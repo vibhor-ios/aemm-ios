4f0e2479b82140a39186828e00bcb3f277e30217
Andrew Zellman <zellman@adobe.com>
2016-10-17 16:32:14 -0700

    Increment version

--- CordovaLib/cordova.js
+++ CordovaLib/cordova.js
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var PLATFORM_VERSION_BUILD_LABEL = '5.0.1';
+var PLATFORM_VERSION_BUILD_LABEL = '5.0.2-dev';
 // file: src/scripts/require.js
 
 /*jshint -W079 */
